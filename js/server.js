313024702b6bda917daeff5056e0d791ee729631
Harsh Davra <harshdavra1@gmail.com>
2025-06-18 19:09:17 +0530

    fix package script

--- js/server.js
+++ js/server.js
@@ -5,6 +5,7 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "../public")));
 app.use('/assets', express.static(path.join(__dirname, "../assets")));
 
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
